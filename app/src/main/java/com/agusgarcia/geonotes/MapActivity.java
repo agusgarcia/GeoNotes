9975f2c292bab061e6cb15d1072370c2b5e915ea
Agustina <geminis_agus@hotmail.com>
2016-02-04 22:00:41 +0100

    GoogleApiClient added as dependency and instantiated in MapActivity

--- app/src/main/java/com/agusgarcia/geonotes/MapActivity.java
+++ app/src/main/java/com/agusgarcia/geonotes/MapActivity.java
@@ -1,4 +1,40 @@
 package com.agusgarcia.geonotes;
 
-public class MapActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MapActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks {
+
+    private static final String TAG = "Map Activity";
+
+    protected GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 }
