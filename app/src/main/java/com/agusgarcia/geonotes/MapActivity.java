0ed545b5c06323cbb0c3de9d72cd82fdea18a0e3
Agustina <geminis_agus@hotmail.com>
2016-02-14 20:50:05 +0100

    Check if network and gps are enabled. Improve splash screen and icon.

--- app/src/main/java/com/agusgarcia/geonotes/MapActivity.java
+++ app/src/main/java/com/agusgarcia/geonotes/MapActivity.java
@@ -1,17 +1,28 @@
 package com.agusgarcia.geonotes;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -25,7 +36,7 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
     protected GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private MapFragment mapFragment;
-
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +78,10 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
     public void onDestroy() {
         super.onDestroy();
     }
+
     @Override
     protected void onStop() {
         if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, mapFragment);
             mGoogleApiClient.disconnect();
         }
         super.onStop();
@@ -82,10 +92,71 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
 
         Log.d(TAG, "Google API Connected");
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+
+        LocationManager mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        ConnectivityManager mNetworkManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            Log.d(TAG, "not enabled");
+
+            new MaterialDialog.Builder(this)
+                    .title("GPS not enabled")
+                    .content("Your GPS is not enabled. Please, enable it.")
+                    .positiveText("Go to settings")
+                    .negativeText("Go back")
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    })
+                    .onNegative(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                            onBackPressed();
+                        }
+                    })
+                    .show();
+        }
+
+
+        NetworkInfo netInfo = mNetworkManager.getActiveNetworkInfo();
+
+        if (netInfo == null || !netInfo.isConnected()) {
+
+            Log.d(TAG, "not enabled");
+
+            new MaterialDialog.Builder(this)
+                    .title("Internet connection")
+                    .content("You have no access to internet. Please, connect you.")
+                    .positiveText("Go to settings")
+                    .negativeText("Go back")
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                            Intent intent = new Intent(Settings.ACTION_SETTINGS);
+                            startActivity(intent);
+                        }
+                    })
+                    .onNegative(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                            onBackPressed();
+                        }
+                    })
+                    .show();
+        }
+
 
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
@@ -104,7 +175,6 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
         locationRequest.setFastestInterval(5000);
 
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, mapFragment);
-
         Log.d(TAG, "OnConnection " + mLastLocation.toString());
 
     }
